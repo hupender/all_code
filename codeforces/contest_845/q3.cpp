3a0cbd47826faa6297a979147897ae6190dc5dfd
hupender <hupenderkhatod>
2023-02-18 00:00:45 +0530

    solved quiz master

--- codeforces/contest_845/q3.cpp
+++ codeforces/contest_845/q3.cpp
@@ -44,55 +44,107 @@ void solve() {
         }   
     }
     if(map1.size()==m) {
-        int flag1=1,flag2=1;
         int st=0,end=n-1;
-        int res;
+        int res=a[end]-a[st];
+        map<int,int> map2,map3;
+        map2=map1;
+        map3=map1;
+        // firstly remove as many elements as possible from begining and then from last and vice-versa
         while(true) {
             // firstly check if we can ignore the first element
+            int flag1=1;
             for(int i=0;i<v[st].size();i++) {
-                if(map1[v[st][i]]<=1) {
+                if(map2[v[st][i]]<=1) {
                     flag1=0;
                 }
             }
-
-            // second check if we can leave the last
-            for(int i=0;i<v[end].size();i++) {
-                if(map1[v[end][i]]<=1) {
-                    flag2=0;
-                }
-            }
-            // if we can remove both check which one is more benificial
-            if(flag1 && flag2) {
-                int diff1=a[end-1]-a[st];
-                int diff2=a[end]-a[st+1];
-                if(diff1<diff2) {
-                    for(int i=0;i<v[end].size();i++) {
-                        map1[v[end][i]]--;
-                    }
-                    end--;
-                }
-                else {
-                    for(int i=0;i<v[st].size();i++) {
-                        map1[v[st][i]]--;
-                    }
-                    st++;
-                }
-            }
-            else if(flag1) {
+            if(flag1) {
                 for(int i=0;i<v[st].size();i++) {
-                    map1[v[st][i]]--;
+                    map2[v[st][i]]--;
                 }
                 st++;
             }
-            else if(flag2) {
+            else {
+                break;
+            }
+        }
+        st--;
+        res=min(res,(a[end]-a[st+1]));
+
+        // now after removing as many number from starting as we can now start removing from end until we can't further
+        while(st>=0 && end>=st) {
+            // check if we can remove last element
+            int flag1=1;
+            for(int i=0;i<v[end].size();i++) {
+                if(map2[v[end][i]]<=1) {
+                    flag1=0;
+                }
+            }
+            if(flag1) {
+                // we can remove last element so remove it
                 for(int i=0;i<v[end].size();i++) {
-                    map1[v[end][i]]--;
+                    map2[v[end][i]]--;
+                }
+                end--;
+                res=min(res,(a[end]-a[st+1]));
+            }
+            else {
+                // we can't remove so add a starting element and then try again
+                for(int i=0;i<v[st].size();i++) {
+                    map2[v[st][i]]++;
+                }
+                st--;
+                res=min(res,(a[end]-a[st+1]));
+            }
+        }
+        // this time start from end
+        st=0,end=n-1;
+
+        while(true) {
+            // firstly check if we can ignore the last element
+            int flag1=1;
+            for(int i=0;i<v[end].size();i++) {
+                if(map3[v[end][i]]<=1) {
+                    flag1=0;
+                }
+            }
+            if(flag1) {
+                for(int i=0;i<v[end].size();i++) {
+                    map3[v[end][i]]--;
                 }
                 end--;
             }
             else {
-                res=a[end]-a[st];
-                break; 
+                break;
+            }
+        }
+        end++;
+        res=min(res,(a[end-1]-a[st]));
+
+        // now after removing as many number from ending as we can , now start removing from start until we can't further
+        while(end<n && st<=end) {
+            // check if we can remove first element
+            int flag1=1;
+            for(int i=0;i<v[st].size();i++) {
+                if(map3[v[st][i]]<=1) {
+                    flag1=0;
+                }
+            }
+            if(flag1) {
+                // we can remove first element so remove it
+                for(int i=0;i<v[st].size();i++) {
+                    map3[v[st][i]]--;
+                }
+                st++;
+                res=min(res,(a[end-1]-a[st]));
+            }
+            else {
+                // we can't remove so add a ending element and then try again
+                for(int i=0;i<v[end].size();i++) {
+                    map3[v[end][i]]++;
+                }
+                end++;
+                res=min(res,(a[end-1]-a[st]));
             }
         }
         cout<<res<<endl;
@@ -115,6 +167,27 @@ int main() {
     while(t--) {
         solve();
     }
+    // for(int i=1;i<=t;i++) {
+    //     if(t==1000) {
+    //         int n,m;
+    //         cin>>n>>m;
+    //         int a[n];
+    //         for(int i=0;i<n;i++) {
+    //             cin>>a[i];
+    //         }
+    //         if(i==36) {
+    //             cout<<n<<" "<<m<<endl;
+    //             for(int i=0;i<n;i++) {
+    //                 cout<<a[i]<<" ";
+    //             }
+    //             cout<<endl;
+    //         }
+    //     }
+    //     else {
+    //         solve();
+    //     }
+    //     // solve();
+    // }
     #ifndef ONLINE_JUDGE
         clock_t clock_end = clock();
         cout<<"Executed In : "<<double(clock_end-clock_begin)/CLOCKS_PER_SEC*1000<<" ms"<<endl;
